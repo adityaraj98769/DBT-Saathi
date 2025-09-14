e80e792f0419b42f0c93279843a472e7e9ad3b24
ADITYA <166215470+adityaraj98769@users.noreply.github.com>
2025-09-14 18:08:19 +0530

    today-quiz section completed

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+document.getElementById("faq-btn").addEventListener("click", function() {
+      window.location.href = "faqs.html";
+});
+document.getElementById("try-quiz").addEventListener("click", function() {
+      window.location.href = "quiz.html";
+});
+document.getElementById("st-quiz").addEventListener("click", function() {
+      window.location.href = "quiz.html";
+});
